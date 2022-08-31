c45249409bbdb43b4b010a7a37d4042b911592b4
A.Ovchinnikov <a.ovchinnikov@sredaobuchenia.ru>
2022-08-31 08:22:55 +0300

    dist

---
+++ assets/AboutView.4224134d.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c,a as t}from"./index.11415d47.js";const n={},s=t("h1",null,"SecondPage",-1),a=[s];function _(r,l){return o(),c("main",null,a)}const i=e(n,[["render",_]]);export{i as default};
--- assets/AboutView.4462c086.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c,a as t}from"./index.f937536d.js";const n={},s=t("h1",null,"SecondPage",-1),a=[s];function _(r,l){return o(),c("main",null,a)}const i=e(n,[["render",_]]);export{i as default};
